1e80bb42a9ce738783d0b63be1590249cf940ece
GitRKhanHub <GitRKhanHub@github.com>
2021-08-23 14:29:43 +0500

    New proj

---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using System.Net;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+
+using System.IO;
+
+namespace App1
+{
+    public class Camera
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Sound { get; set; }
+        public string ParentName { get; set; }
+
+    }
+    public partial class App : Application
+    {
+        public static List<Camera> Cameras;
+        public static string Spisok;
+
+        
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            Cameras = CameraListCreator();
+            Spisok = MakeSpisok();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        public static XDocument GetReply_from_http() // получаем конфигурацию по http запросу в виде XDocument
+        {
+            WebClient client = new WebClient();
+            string address = "http://demo.macroscop.com/configex?login=root";
+            string reply = client.DownloadString(address);
+
+            reply = reply.Remove(0, reply.IndexOf("<"));
+
+            XDocument My_XmlDoc = XDocument.Parse(reply);
+
+            return My_XmlDoc;
+        }
+
+        public static List<Camera> CameraListCreator()
+        {
+
+            XDocument Xdoc = GetReply_from_http();
+            List<Camera> cameras = new List<Camera>();
+
+            
+            foreach (XElement el in Xdoc.Element("Configuration").Element("Channels").Elements("ChannelInfo"))
+            {
+                Camera C = new Camera();
+                C.Id = el.Attribute("Id").Value;
+                C.Name = el.Attribute("Name").Value;
+                C.Sound = el.Attribute("IsSoundOn").Value;
+                foreach (XElement elel in Xdoc.Element("Configuration").Element("RootSecurityObject").Element("ChildSecurityObjects").
+                    Elements("SecObjectInfo"))
+                {
+                    foreach (XElement elelel in elel.Element("ChildChannels").Elements("ChannelId"))
+                    {
+                        if (elelel.Value == C.Id)
+                        {
+                            C.ParentName = elel.Attribute("Name").Value;
+                        }
+                    }
+                }
+                cameras.Add(C);
+            }
+            return cameras;
+
+        }
+
+        public static string MakeSpisok()
+        {
+            string temp = "";
+            int i = 1;
+            foreach (Camera c in Cameras)
+            {
+                temp += i + ") " + c.Name + ", " + c.Sound + ", " + c.ParentName + "\n";
+                i++;
+            }
+            return temp;
+        }
+    }
+}
---
+++ App1/App1/Page2.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using System.Net;
+using System.IO;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        //static Image image = new Image() { Source = "examp.jpg"};
+        //static ImageSource MySource = image.Source;
+        public static ImageSource MySource;
+
+        public Page2()
+        {
+            InitializeComponent();
+            
+        }
+
+
+
+
+
+
+
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Получить изображение";
+
+            WebClient webClient = new WebClient();
+            string myurl = "http://demo.macroscop.com/mobile?channelid=" +
+                App.Cameras[0].Id + "&oneframeonly=true&login=root";
+            Stream stream = webClient.OpenRead(myurl);
+
+
+            MySource = ImageSource.FromStream(() => stream);
+            myLabeltwo.Text = "Кадр с камеры: " +  App.Cameras[0].Name;
+            myImage.Source = MySource;
+            
+
+
+        }
+
+
+    }
+}
